79a20f89cfa85f5db57200ec4ca8341797c39f2e
Amy <G00235358@gmit.ie>
2021-03-24 19:05:42 +0000

    Week02_Push1

---
+++ bmi.py
@@ -0,0 +1,18 @@
+# bmi.py
+# Weekly Task 02 - BMI Calculation 
+# Author: Amy Reynolds 
+
+# request person's height in centrimeters 
+# int - converts string that inputted into to an integer
+height = int(input ("enter your height (cm):? "))
+print ("your height (cm) is {} " .format (height))
+
+# request person's weight in kg         
+weight = int(input ("enter your weight (kg):?"))
+print ("your weight (kg) is {} " .format (weight))  
+
+# BMI Calculation (kg/m^2)
+BMI = weight / (height/100)**2
+
+# BMI Calculation Result Output (weight / height)
+print ("your BMI is {} " .format (BMI)) 
