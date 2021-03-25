bc7a66a65380e6cb41652f3b9d899671e5a30794
Amy <G00235358@gmit.ie>
2021-03-25 15:26:02 +0000

    Week02_Push04_Ref

--- bmi.py
+++ bmi.py
@@ -1,6 +1,10 @@
 # bmi.py
 # Weekly Task 02 - BMI Calculation 
-# Author: Amy Reynolds 
+# Author: Amy Reynolds
+
+# References
+# Ln 19 - https://www.includehelp.com/python/bmi-body-mass-index-calculator.aspx - BMI Calculation
+# Ln 25 - https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points - Limit BMI Result to 2 dec.    .format(round answer, 2) 
 
 # request person's height in centrimeters 
 # int - converts string that inputted into to an integer
@@ -18,9 +22,4 @@ BMI = weight / (height/100)**2
 print ("your BMI is {} " .format (BMI)) 
 
 # display / round to 2 decimal places 
-print ("your BMI is {} " .format(round (BMI),2))
-
-
-# References
-# LN 15 - https://www.includehelp.com/python/bmi-body-mass-index-calculator.aspx - BMI Calculation 
-# Ln 21 - https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points - Limit BMI Result to 2 dec.    .format(round answer, 2) 
+print ("your BMI is {} " .format(round (BMI , 2)))
