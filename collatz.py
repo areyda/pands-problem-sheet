d6d19ff983ac469911a02669e44fc454d6999f8a
Amy <G00235358@gmit.ie>
2021-04-16 18:02:39 +0100

    Week04_Push02

--- collatz.py
+++ collatz.py
@@ -2,13 +2,22 @@
 # Weekly Task 04 - collatz.py
 # Author: Amy Reynolds
 
-numbers = []
+# References
+# Ln 13-25 - https://www.reddit.com/r/Python/comments/57r6bf/collatz_conjecture_program/
 
-number= int(input ("Please enter a number: "))
+# Request the user to input a positive integer
+number = int(input ("Please enter a positive integer: "))
 
-if (number%2) == 0:
-    print(int(number/2),(numbers))
-
-else:print(int(number*3+1),(numbers))
-
-print(numbers)
+# Continue looping until the number = 1
+    # Output the current value of the number 
+while number!=1:
+    print (number)
+    # Using the modulo operator with a modulus of 2, to determine if the number is even
+    # Divide the number by 2 (// is used to ensure that an integer value is outputted instead of a float)
+    if (number%2) == 0:
+        number = (number//2)
+    # Otherwise (in this case if the number is odd), multiply the number by 3 and add 1.
+    else:
+        number= ((number*3)+1)
+# Output number (in this case the value is 1 - outside of while loop)
+print(number)
